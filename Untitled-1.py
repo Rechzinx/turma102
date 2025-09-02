d3526a0366127daaafe4dea7f788be015605793b
Mayron <costela@exemplo.com>
2025-09-02 08:34:16 -0300

    teste

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número: "))
+if num == 7:
+    print("igual a 7")
+else:
+    print("diferente de 7")
---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um número: "))
+
+if num > 5:
+    print("maior que 5")
+else:
+    print("menor ou igual a 5")
+
